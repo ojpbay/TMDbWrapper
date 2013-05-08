fc7a3a874ba25253eb6e964446f18c3fa83bd9c2
Oliver Baylis <oli.baylis@gmail.co.uk>
2013-05-08 20:57:40 +0100

    Add test project.

---
+++ TmdbWrapper.Tests/TmdbWrapper.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+
+namespace TmdbWrapper.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            TheMovieDb.Initialise("TODO");
+
+            var results = await TheMovieDb.SearchMovieAsync("lebowski");
+
+            if (results != null)
+            {
+            }
+        }
+    }
+}
